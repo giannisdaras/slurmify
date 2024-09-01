3d08159d3408e7428a4eee814fad5f240ba5f24d
giannisdaras <daras.giannhs@gmail.com>
2024-09-01 12:03:35 -0500

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="slurmify",
-    version="0.1.1",
+    version="0.1.3",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "click",  # for CLI interface
